e04926ac58cec8be95ba8e71abd63136d05cc886
Sergio Livi <sergio.asanmichele@gmail.com>
2013-07-25 18:21:30 +0200

    publish details

--- config.sample.py
+++ config.sample.py
@@ -11,10 +11,10 @@ config = {
   "Alarms": {
     #"ConsoleAlarm": {}, testing purposes
     "CommandAlarm": {
-      "command": "echo 'an alarm' #" # i'll call command 'event name', so if not interested, end command with #
+      "command": "echo 'an alarm' #" # i'll call "command 'event name'", so if not interested, end command with #
     },
     "Mp3Alarm": {
-      "command": "mpg321 -g 100",
+      "command": "mplayer -quiet", #"mpg321 -g 100",
       "directory": "path/to/dir" # I will choose one random file in the directory
     }
   }
